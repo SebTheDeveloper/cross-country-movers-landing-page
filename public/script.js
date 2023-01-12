e090372810d1f674d62e9a5fa5b213ccbf5dd6c4
sebdoubleu <111027905+sebdoubleu@users.noreply.github.com>
2023-01-12 02:40:23 -0500

    add granot compatibility

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
 const form = document.querySelector('form');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  window.open('https://www.crosscountrymovers.info', '_self');
-});
+    form.addEventListener('submit', () => {
+      const movedte = document.querySelector('#movedte');
+      const dateInput = document.querySelector('#date');
+      const mmdd = movedte.value.slice(5);
+      const yyyy = movedte.value.slice(0, 4);
+      const date = `${mmdd}-${yyyy}`;
+
+      dateInput.value = date;
+    });
